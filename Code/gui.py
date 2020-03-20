6b8fed1aa0bec201e60679b65615af6639bd5dab
Alvin <alvinng2707@gmail.com>
2020-03-21 01:51:28 +0800

    Added timer module

--- Code/gui.py
+++ Code/gui.py
@@ -1,6 +1,28 @@
 import tkinter as tk
 import scramble
 
+cubeTime = 0.00
+timing = False
+
+def startTimer(event):
+    global timing, cubeTime
+    timing = True
+    cubeTime = 0.00
+    window.bind("<space>", stopTimer)
+    
+    def start():
+        if timing == True:
+            global cubeTime
+            cubeTime += 0.01
+            timeLbl.config(text = round(cubeTime, 3))
+            timeLbl.after(10, start)
+    start()
+
+def stopTimer(event):
+    global timing
+    timing= False
+    window.bind("<space>", startTimer)
+
 window = tk.Tk()
 window.title("Cube Timer")
 window.iconbitmap("icon.ico")
@@ -13,8 +35,12 @@ window.state("zoomed")
 
 #Scarmble label
 scramble_Text = tk.StringVar()
-scrambleLbl = tk.Label(window, textvariable = scramble_Text, font = ("Calibri", 20))
-scrambleLbl.place(relx = 0.26)
+scrambleLbl = tk.Label(window, textvariable = scramble_Text, font = ("Calibri", 30))
+scrambleLbl.place(relx = 0.16)
+
+#Time label
+timeLbl = tk.Label(window, text = round(cubeTime, 3), font = ("Calibri", 100))
+timeLbl.place(relx = 0.4, rely = 0.36)
 
 #Calling the scramble generator
 def scrambler():
@@ -23,7 +49,9 @@ def scrambler():
 scrambler()
 
 #Btn for generating new scramble
-btn1 = tk.Button(window, text = "New scramble", command = scrambler, font = ("Calibri", 15))
-btn1.place(relx = 0.43, rely = 0.05)
+btn1 = tk.Button(window, text = "New scramble", command = scrambler, font = ("Calibri", 20))
+btn1.place(relx = 0.43, rely = 0.07)
+
+window.bind("<space>", startTimer)
 
 window.mainloop()
